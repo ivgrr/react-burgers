9f10e25e508f563e2c856519ed234d42ed3f597c
ivgrr <gameapbivan@gmail.com>
2021-08-30 10:06:17 +0300

    add-component-MenuAdmin

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Header from "./Header";
 import Order from "./Order";
-
+import MenuAdmin from "./MenuAdmin";
 
 class App extends React.Component {
     render() {
         return(
             <div className='burger-paradise'>
                 <div className='menu'>
-                    <Header />
+                    <Header title='Very Hot Burger'/>
                 </div>
                 <Order />
-                {/*<MenuAdmin />*/}
+                <MenuAdmin />
             </div>
         );
     }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,52 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className='top'>
-                <div className='wrap'>
-                    <div className='header-content'>
+const Header = props => (
+        <header className='top'>
+            <div className='wrap'>
+                <div className='header-content'>
 
-                        <div className='header-rating'>
-                            <div className='header-rating_tag'>Рейтинг</div>
-                            <div className='header-rating_icon'>+++</div>
-                        </div>
-                        <div className='header-divider'></div>
-                        <h1 className='font-effect-fire-animation'>Hot Burgers</h1>
-                        <h3>
+                    <div className='header-rating'>
+                        <div className='header-rating_tag'>Рейтинг</div>
+                        <div className='header-rating_icon'>★★★★★</div>
+                    </div>
+                    <div className='header-divider'> </div>
+                    <h1 className='font-effect-fire-animation'>{props.title}</h1>
+                    <h3>
                             <span>
                                 Быстрая доставка горячих
                                 <span className='sub-header'> #бургеров</span>
                             </span>
-                        </h3>
-                    </div>
+                    </h3>
                 </div>
-            </header>
-        );
-    }
-}
+            </div>
+        </header>
+)
+
+
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header className='top'>
+//                 <div className='wrap'>
+//                     <div className='header-content'>
+//
+//                         <div className='header-rating'>
+//                             <div className='header-rating_tag'>Рейтинг</div>
+//                             <div className='header-rating_icon'>★★★★★</div>
+//                         </div>
+//                         <div className='header-divider'> </div>
+//                         <h1 className='font-effect-fire-animation'>{this.props.title}</h1>
+//                         <h3>
+//                             <span>
+//                                 Быстрая доставка горячих
+//                                 <span className='sub-header'> #бургеров</span>
+//                             </span>
+//                         </h3>
+//                     </div>
+//                 </div>
+//             </header>
+//         );
+//     }
+// }
 
 export default Header;
