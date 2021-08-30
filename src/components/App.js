a57a9a42cd99ccd6f15b3ee68c5f24335a063df5
ivgrr <gameapbivan@gmail.com>
2021-08-30 21:08:00 +0300

    add-view-burgers

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from "./Header";
 import Order from "./Order";
 import MenuAdmin from "./MenuAdmin";
+import sampleBurgers from "../sample-burgers";
+import Burger from "../Burger";
 
 class App extends React.Component {
 
@@ -13,20 +15,33 @@ class App extends React.Component {
     addBurger = burger => {
         // Делаем копию объекта state
         const burgers = {...this.state.burgers};
-        // Добавить ноый бургер в переменную burgers
+        // Добавить новый бургер в переменную burgers
         burgers[`burger${Date.now()}`] = burger;
         // Записать наш новый объект burgers в state
         this.setState({ burgers });
+    };
+
+    loadSampleBurgers = () => {
+        this.setState({burgers: sampleBurgers});
     };
 
     render() {
         return(
             <div className='burger-paradise'>
                 <div className='menu'>
-                    <Header title='Very Hot Burger'/>
+                    <Header title='Hot Burgers'/>
+                    <ul className='burgers'>
+                        {Object.keys(this.state.burgers).map(key => {
+                            return <Burger key={key}
+                                           index={key}
+                                           details={this.state.burgers[key]}
+                            />
+                        })}
+                    </ul>
                 </div>
                 <Order />
-                <MenuAdmin addBurger = {this.addBurger}/>
+                <MenuAdmin addBurger = {this.addBurger}
+                           loadSampleBurgers={this.loadSampleBurgers}/>
             </div>
         );
     };
--- src/components/MenuAdmin.js
+++ src/components/MenuAdmin.js
@@ -7,9 +7,10 @@ class MenuAdmin extends React.Component {
             <div className='menu-admin'>
                 <h2>Управление меню</h2>
                 <AddBurgerForm addBurger={this.props.addBurger} />
+                <button onClick={this.props.loadSampleBurgers}>Загрузить бургеры</button>
             </div>
         );
-    }
+    };
 }
 
 export default MenuAdmin
