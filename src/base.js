446319c02dbb4d4760369120dc6ad2dae57cf2b0
ivgrr <gameapbivan@gmail.com>
2021-09-01 02:57:03 +0300

    integration-google-firebase

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import Rebase from 're-base';
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyCr6-jgZtPXNCFBQYuBu5RAJCEnQF5O4ew",
+    authDomain: "very-hot-burgers-e9265.firebaseapp.com",
+    databaseURL: "https://very-hot-burgers-e9265-default-rtdb.firebaseio.com",
+})
+
+const base = Rebase.createClass(firebase.database());
+
+export {firebaseApp};
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,25 @@ import Order from "./Order";
 import MenuAdmin from "./MenuAdmin";
 import sampleBurgers from "../sample-burgers";
 import Burger from "../Burger";
+import base from "../base";
 
 class App extends React.Component {
 
     state = {
         burgers: {},
         order: {}
+    };
+
+    componentDidMount() {
+        const {params} = this.props.match;
+        this.ref = base.syncState(`${params.restaurantId}/burgers`, {
+            context: this,
+            state: 'burgers'
+        });
+    };
+
+    componentWillUnmount() {
+        base.removeBinding(this.ref)
     };
 
     addBurger = burger => {
